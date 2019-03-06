417d32cd072bff8c2d1ac2dbcdd2e9d57ea79f28
NilTestudo <31516843+NilTestudo@users.noreply.github.com>
2019-03-06 19:14:25 +0100

    Commit of Initial Project

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Game
+{
+    private static GameState state;
+
+    public static GameState State
+    {
+        get
+        {
+            return state;
+        }
+
+        set
+        {
+            state = value;
+        }
+    }
+}
